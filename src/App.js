e77a6b37d448faf77247c57c4c95aac34fca20ee
aliceprados07-max <alice.prados07@gmail.com>
2025-08-25 22:00:08 -0300

    Setimo commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import "./App.css"
 
-function Square ({valor}){
+function Square (){
+  const [valor, setValor] = useState(null);
   function handleClick(){
-    console.log("Botão clicado");
+    setValor("X");
   }
   return(
     <button className="square" onClick={handleClick}>
@@ -14,19 +16,19 @@ export default function Tabuleiro(){
   return(
     <div>
       <div>
-        <Square valor="1"/>
-         <Square valor="2"/>
-         <Square valor="3"/>
+        <Square />
+         <Square />
+         <Square />
     </div>
     <div>
-    <Square valor="4"/>
-    <Square valor="5"/>
-    <Square valor="6"/>
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div>
-    <Square valor="7"/>
-    <Square valor="8"/>
-    <Square valor="9"/>
+      <Square />
+      <Square />
+      <Square />
     </div>
     </div>
   )

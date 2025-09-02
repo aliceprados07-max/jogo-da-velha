520311ffa6af1f995508f9a1dcc8b2c022c89519
Alice <alice.prados07@gmail.com>
2025-09-01 21:33:19 -0300

    identificação de vencedores

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ export default function Tabuleiro(){
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(Array(9).fill(null));
   function handleClick(i){
-    if(squares[i])// se square de i é null o if não executa o return
-      return;
+    if(squares[i] || haVencedor(squares)){
+      return};
       //o handleClick continua a execução pois o return não foi executado o squares[i] era null
       const nextSquares = squares.slice();
       if(xIsNext)
@@ -24,8 +24,17 @@ export default function Tabuleiro(){
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+  const vencedor = haVencedor(squares);  
+  const empate = !vencedor && squares.every(square => square !== null);
   return(
     <div>
+      <h2>
+        {vencedor
+          ? `Vencedor: ${vencedor}`
+          : empate
+          ? "Empate!"
+          : `Próximo jogador: ${xIsNext ? "X" : "O"}`}
+      </h2>
       <div>
         <Square valor={squares[0]} onSquareClick = {() => {handleClick(0)}}/>
          <Square valor={squares[1]} onSquareClick = {() => {handleClick(1)}}/>
@@ -43,6 +52,29 @@ export default function Tabuleiro(){
     </div>
     </div>
   )
+}
+
+function haVencedor(squares)
+{
+   // Todas as combinações vencedoras possíveis
+  const linhasVencedoras = [
+    [0, 1, 2], // linha 1
+    [3, 4, 5], // linha 2
+    [6, 7, 8], // linha 3
+    [0, 3, 6], // coluna 1
+    [1, 4, 7], // coluna 2
+    [2, 5, 8], // coluna 3
+    [0, 4, 8], // diagonal principal
+    [2, 4, 6]  // diagonal secundária
+  ];
+
+  for (let combinacao of linhasVencedoras) { //array com com todas as combinações para ganhar
+    const [a, b, c] = combinacao; //pega os três números da combinação e guarda em a, b, c
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { //garante que todos tem o mesmo valor 
+      return squares[a]; // Retorna "X" ou "O"
+    }
+  }
+  return null; // Nenhum vencedor ainda
 }
 
 
